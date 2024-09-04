adb3b287cf9cf2b9b6238071bd881e2e965b44fa
EnjoyZz <130199362@qq.com>
2024-09-04 07:39:30 +0000

    Revert "修复数据库编码不一致的问题"
    This reverts commit f1d89a3aa99f37a7a1e7e8f63c4f16f88e84cebb.

--- resources/views/standard/edit.blade.php
+++ resources/views/standard/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.edit')
 @section('content_form')
-    <div class="col-12 div-name">
+    <div class="col-12">
         <div class="sidebar-heading mt-3 mb-2">标准</div>
         <input type="text" name="name" class="form-control" value="{{$standard->name}}">
     </div>
