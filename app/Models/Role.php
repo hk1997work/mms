e07ca0c1e3513a7818bb00b0f0ab7c2555dede3b
130199362@qq.com <130199362@qq.com>
2025-08-19 16:56:48 +0800

    2.0

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,22 +12,4 @@ class Role extends Model
     protected $fillable = [
         'name',
     ];
-
-    //当前角色所有用户
-    public function users()
-    {
-        return $this->belongsToMany(User::class, 'role_user', 'role_id', 'user_id')->withPivot(['user_id', 'role_id'])->orderBy('user_id');
-    }
-
-    //当前角色所有权限
-    public function permissions()
-    {
-        return $this->belongsToMany(Permission::class, 'permission_role', 'role_id', 'permission_id')->withPivot(['permission_id', 'role_id'])->orderBy('sort');
-    }
-
-    //当前角色所有岗位
-    public function positions()
-    {
-        return $this->belongsToMany(Position::class, 'position_role', 'role_id', 'position_id')->withPivot(['position_id', 'role_id'])->orderBy('sort');
-    }
 }
