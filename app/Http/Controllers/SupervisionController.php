54e6abcb3c87096dfd33e38c6c31454098cf0751
130199362@qq.com <130199362@qq.com>
2025-10-16 09:41:13 +0800

    2.0

--- app/Http/Controllers/SupervisionController.php
+++ app/Http/Controllers/SupervisionController.php
@@ -9,19 +9,16 @@ class SupervisionController extends Controller
     public function show($id, $output = false)
     {
         $certificates = CertificatesView::where('valid', 1)->whereIn('id', explode(',', $id))->orderBy('order')->get();
-        $unit = '';
-        $position = '';
+        $arr = [];
         $str = "计量器具检查：";
         foreach ($certificates as $certificate) {
-            if ($certificate->position4 != $unit) {
-                $str = $str . "<br>" . $certificate->position4 . "：";
+            $arr[$certificate->position4][$certificate->position1][] = $certificate->instrument . "，编号：" . $certificate->number;
+        }
+        foreach ($arr as $key => $value) {
+            $str = $str . "<br>" . $key . "：";
+            foreach ($value as $key1 => $value1) {
+                $str = $str . "<br>" . $key1 . "岗位：" . implode('；', $value1) . "；";
             }
-            if ($certificate->position1 != $position) {
-                $str = $str . "<br>" . $certificate->position1 . "岗位：";
-            }
-            $str = $str . $certificate->instrument . "，编号：" . $certificate->number . "；";
-            $unit = $certificate->position4;
-            $position = $certificate->position1;
         }
         $str = $str . "<br>以上计量器具满足使用要求，标签完好，与计量器具台账一致。";
         return $output ? $str : view('supervision.show', compact('str'));
